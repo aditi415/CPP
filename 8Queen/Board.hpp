2dc01a4e046b7a06bd721449b0b5d169394932eb
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-03-21 19:33:10 +0530

    Board.hpp

--- 8Queen/Board.hpp
+++ 8Queen/Board.hpp
@@ -1,1 +1,21 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "Queen.h"
+#include <vector>
+#include <iostream>
+
+class Board {
+private:
+    std::vector<std::vector<char>> board;
+    static const int SIZE = 8;
+
+public:
+    Board();
+    bool isSafe(int row, int col);
+    bool solve(int col);
+    void display();
+};
+
+#endif
 
