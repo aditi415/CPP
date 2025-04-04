b0491e25023ffecc460511f5b8b4fbdb424203ac
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-04-04 20:58:56 +0530

    Symmetric_date.hpp

--- Symmetric Dates in century 2001-2100         Eg-    05-02-2050/Symmetric_date.hpp
+++ Symmetric Dates in century 2001-2100         Eg-    05-02-2050/Symmetric_date.hpp
@@ -1,1 +1,19 @@
+#ifndef SYMMETRIC_DATE_HPP
+#define SYMMETRIC_DATE_HPP
 
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+
+class SymmetricDateFinder {
+public:
+    void findSymmetricDates();
+
+private:
+    bool isSymmetric(const std::string& date);
+    bool isValidDate(int day, int month, int year);
+    bool isLeapYear(int year);
+};
+
+#endif // SYMMETRIC_DATE_FINDER_HPP
