09c5418cc0fc7a82dc30dbb05fa165987e0757f0
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-05-05 12:10:17 +0530

    lagranges.hpp

---
+++ Develop a C++ progran1 to find the function value at any point z ¬ (20,l given a set of 2D points |2;. f(ri)): i= 0,1,2, ..n. You may make use of Langrangian interpolation: j=n j=l,j#i 0 (z-a) T;-T;/lagranges.hpp
@@ -0,0 +1,8 @@
+#ifndef LAGRANGE_HPP
+#define LAGRANGE_HPP
+
+#include <vector>
+
+double lagrangeInterpolation(const std::vector<double>& x, const std::vector<double>& y, double z);
+
+#endif // LAGRANGE_HPP
