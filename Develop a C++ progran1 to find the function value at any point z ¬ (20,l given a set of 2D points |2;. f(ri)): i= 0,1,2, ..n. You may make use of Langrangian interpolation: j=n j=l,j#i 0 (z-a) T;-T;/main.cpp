6f6ab2fca1d86f8d331465f16d3066148d24e609
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-05-05 12:10:52 +0530

    main.cpp

---
+++ Develop a C++ progran1 to find the function value at any point z ¬ (20,l given a set of 2D points |2;. f(ri)): i= 0,1,2, ..n. You may make use of Langrangian interpolation: j=n j=l,j#i 0 (z-a) T;-T;/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include "lagrange.hpp"
+
+int main() {
+    int n;
+    std::cout << "Enter the number of data points: ";
+    std::cin >> n;
+
+    std::vector<double> x(n), y(n);
+    std::cout << "Enter the x and f(x) values:\n";
+    for (int i = 0; i < n; ++i) {
+        std::cout << "x[" << i << "] = ";
+        std::cin >> x[i];
+        std::cout << "f(x[" << i << "]) = ";
+        std::cin >> y[i];
+    }
+
+    double z;
+    std::cout << "Enter the point z at which f(z) is to be estimated: ";
+    std::cin >> z;
+
+    double result = lagrangeInterpolation(x, y, z);
+    std::cout << "The estimated value f(" << z << ") is: " << result << std::endl;
+
+    return 0;
+}
