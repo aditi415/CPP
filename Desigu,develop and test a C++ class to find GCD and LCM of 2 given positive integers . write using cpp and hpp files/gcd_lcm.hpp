8fe0cafbbb5afa3c832aec2db12f60d64e7fe955
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-05-05 13:25:50 +0530

    gcd_lcm.hpp

---
+++ Desigu,develop and test a C++ class to find GCD and LCM of 2 given positive integers . write using cpp and hpp files/gcd_lcm.hpp
@@ -0,0 +1,15 @@
+#ifndef GCD_LCM_HPP
+#define GCD_LCM_HPP
+
+class NumberOperations {
+private:
+    int a, b;
+
+public:
+    NumberOperations(int x, int y);
+
+    int getGCD() const;
+    int getLCM() const;
+};
+
+#endif // GCD_LCM_HPP
