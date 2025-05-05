07ab4d18bcc1b4644c74b2d67ed9dffb30f6abfd
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-05-05 13:29:41 +0530

    leapyear.cpp

---
+++ Design, develop und test a C++ class to determine if the given 4-digit positive integer repreets a leap year or not/leapyear.cpp
@@ -0,0 +1,11 @@
+#include "leap_year.hpp"
+
+YearChecker::YearChecker(int y) {
+    year = y;
+}
+
+bool YearChecker::isLeapYear() const {
+    if (year % 4 != 0) return false;
+    if (year % 100 != 0) return true;
+    return (year % 400 == 0);
+}
